48489a5e59e27f283239d5077e25ec74cb54f296
Kitsun <kitsunies@gmail.com>
2020-10-18 22:46:05 +1100

    Update events.lua

--- modules/events.lua
+++ modules/events.lua
@@ -1,18 +1,31 @@
+local json = require('json')
+local fs = require('fs')
+
+local cfg = json.decode(fs.readFileSync('config.json'))
+local prefix = cfg.prefix
+
+-- TODO: Create better module management
+local modules = {
+    commands = require("./commands")   
+}
+
 local events = {}
 
 function events.ready(client)
     client:setGame {
         type = 3, 
-        name = idk.."help"
+        name = prefix.."help"
     }
 end
 
 function events.messageCreate(client, msg)
+    modules.commands.create()
     -- ...
 end
 
 
 function events.messageDelete(client, msg)
+    modules.commands.delete()
     -- ...
 end
 
