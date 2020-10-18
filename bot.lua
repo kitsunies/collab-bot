3c407666f6fce32d4a0ba22fb51fa76a344fb218
Kitsun <kitsunies@gmail.com>
2020-10-18 18:49:03 +1100

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -4,9 +4,9 @@ local client = discordia.Client()
 local eventHandler = require('./modules/events')
 
 for k, v in pairs(eventHandler) do
-	client:on(k, function(...) 
-		v(client, ...)
-	end)
+    client:on(k, function(...) 
+        v(client, ...)
+    end)
 end
 
 client:run("Bot SECRETSyUFFS")
