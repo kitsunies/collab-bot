4974959e5269e4fbd12865704e3db560d4eae202
canada <73051771+NotSoClassy@users.noreply.github.com>
2020-10-18 17:51:50 -0400

    Create pp.lua
    nice

---
+++ modules/commands/pp.lua
@@ -0,0 +1,12 @@
+return {
+    run = function(arg, msg)
+       	msg:reply{
+          embed = {
+            title = 'Nice cock bro',
+            description = msg.author.name..'\'s pp:\n8'..string.rep('=', math.random(3, 16)..'D')
+          }
+        }
+    end, 
+    description = 'Get the size of your pp', 
+    aliases = {'penis', 'ppsize'}
+}
