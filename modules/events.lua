4c5ee48df34444199a9a9ecfafbda3d56aa787df
Kitsun <kitsunies@gmail.com>
2020-10-18 18:30:32 +1100

    Update events.lua

--- modules/events.lua
+++ modules/events.lua
@@ -1,7 +1,10 @@
 local events = {}
 
 function events.ready(client)
-    -- ...
+    client:setGame {
+        type = 3, 
+        name = idk.."help"
+    }
 end
 
 function events.messageCreate(client, msg)
