f29a690fbc73927338353f6caeee4afb7c110b66
canada <73051771+NotSoClassy@users.noreply.github.com>
2020-10-18 02:38:35 -0400

    package

---
+++ package.lua
@@ -0,0 +1,14 @@
+return {
+	name = "colab-bot",
+	version = "0.0.1",
+	description = "idk lol",
+	tags = { "discord", "discordia" },
+	license = " Apache-2.0",
+	author = { name = "Kitsun", email = "kitsunies@gmail.com" },
+	homepage = "https://github.com/kitsunies/colab-bot",
+	dependencies = {
+		"SinisterRectus/discordia@2.8.4"
+	},
+	files = {"**.lua"}
+}
+  
