2f08c49c32700718fd1ae1d0862a9d85bb4dd113
Kitsun <kitsunies@gmail.com>
2020-10-18 18:25:40 +1100

    Create events.lua

---
+++ modules/events.lua
@@ -0,0 +1,16 @@
+local events = {}
+
+function events.ready(client)
+    -- ...
+end
+
+function events.messageCreate(client, msg)
+    -- ...
+end
+
+
+function events.messageDelete(client, msg)
+    -- ...
+end
+
+return events
