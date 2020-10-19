e41be50d8aa13368c0319f7f31b76f9000e6e861
canada <73051771+NotSoClassy@users.noreply.github.com>
2020-10-18 22:02:35 -0400

    Indents were also weird

--- modules/commands/pp.lua
+++ modules/commands/pp.lua
@@ -1,10 +1,10 @@
 return {
     run = function(arg, msg)
        	msg:reply{
-          embed = {
-            title = 'Nice cock bro',
-            description = msg.author.name..'\'s pp:\n8'..string.rep('=', math.random(3, 16))..'D'
-          }
+            embed = {
+                title = 'Nice cock bro',
+                description = msg.author.name..'\'s pp:\n8'..string.rep('=', math.random(3, 16))..'D'
+            }
         }
     end, 
     description = 'Get the size of your pp', 
