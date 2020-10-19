aca58e645b492b24dafdac0248cad4aa529b4bab
canada <73051771+NotSoClassy@users.noreply.github.com>
2020-10-19 18:27:49 -0400

    Create clap.lua

---
+++ modules/commands/clap.lua
@@ -0,0 +1,8 @@
+return {
+    run = function(arg, msg)
+       	if not arg then return msg:reply('Nothing to clapify') end
+        msg:reply(arg:gsub('%S+', function(str) return ' 👏 '..str end))
+    end, 
+    description = 'Make 👏 text 👏 like 👏 this', 
+    aliases = {}
+}
