ae8e5b77c98f5aa13db6b529d46f666ebde60c2d
canada <73051771+NotSoClassy@users.noreply.github.com>
2020-10-19 22:53:33 -0400

    Update dadjoke.lua

--- modules/commands/dadjoke.lua
+++ modules/commands/dadjoke.lua
@@ -5,7 +5,7 @@ return {
         local _, body = http.request('GET', 'https://icanhazdadjoke.com/', {{'Accept','text/plain'}})
         msg:reply(body or 'There was an error with the api')
     end, 
-    description = 'Get a hilarious joke from [Dad Joke Api](https://icanhazdadjoke.com/)', 
+    description = 'Get a hilarious joke from [This Api](https://icanhazdadjoke.com/)', 
     group = 'fun',
     aliases = {'joke'}
 }
